c3ad4d8916b1ea894cc79bceee4aa47b98d0ba28
Zach Whalen <zach.whalen@gmail.com>
2020-06-30 17:26:13 -0400

    Update frankenpoem.py

--- poem/frankenpoem.py
+++ poem/frankenpoem.py
@@ -1,41 +1,61 @@
 import random
 from textblob import TextBlob
+import nltk
+nltk.download('punkt')
+nltk.download('averaged_perceptron_tagger')
 
-# This looks for a file called "frankenstein.txt" 
-# in the same directory as my python notebook or script.
-# Replace "frankenstein.txt" with your file's name.
-# The text file's contents are stored in the variable "text"
-# These are comments
-with open('frankenstein.txt', 'r') as file:
-    text = file.read()
+with open("birds_of_australia.txt") as f:
+  bird_text = f.read()
 
-# This parses the text file contents into a TextBlob oject called "blob"
-blob = TextBlob(text)
+bird_blob = TextBlob(bird_text)
 
-# Create two empty lists for storing adjectives and nouns
+poem_text = """
+This is just to say
+I have eaten
+the plums
+that were in
+the icebox,
+
+and which
+you were probably
+saving
+for breakfast.
+
+Forgive me
+they were delicious -- 
+so sweet
+and so cold.
+"""
+
+poem_blob = TextBlob(poem_text)
+
+singular_nouns = []
+plural_nouns = []
 adjectives = []
-nouns = []
-verbs = []
 
-# TextBlob parses the words and labels them with a part-of-speech tag.
-# This code loops through the whole text, checks for adjectives and nouns
-# and adds them to the appropriate list.
-for word,pos in blob.tags:
-    # print(word,pos)
-    if (pos == 'JJ'):
-        adjectives.append(word)
-    if (pos == 'NN'):
-        nouns.append(word)
-    if (pos == 'VB'):
-        verbs.append(word)
+for word,pos in bird_blob.tags:
+  if(pos == 'NN'):
+    singular_nouns.append(word)
 
-# This generates an eight-line poem by pairing a random adjective 
-# with a random noun eight times and printing the pairs.
-for i in range(8):
-    a1 = random.choice(adjectives)
-    a2 = random.choice(adjectives)
-    n1 = random.choice(nouns)
-    n2 = random.choice(nouns)
-    n3 = random.choice(nouns)
-    vb = random.choice(verbs)
-    print(n1 + " " + a1 + " " +  vb  + " " + a2  + " " + n3)
+  if(pos == 'NNS'):
+    plural_nouns.append(word)
+    
+  if(pos == 'JJ'):
+    adjectives.append(word)
+
+
+print("\n\nThis is just to say")
+print("I have eaten")
+print("the " + random.choice(plural_nouns))
+print("that " + random.choice(adjectives) + " were in")
+print("the " + random.choice(singular_nouns))
+print("")
+print("and which")
+print("you were probably")
+print("saving")
+print("for breakfast")
+print("")
+print("Forgive me")
+print("they were " + random.choice(adjectives))
+print("so " + random.choice(adjectives))
+print("and so " + random.choice(adjectives))
